9fb35dc7abde42d6bd5c90fcfd21823d9b7f1ee2
Melyn Atieno Were <melynatieno@gmail.com>
2022-07-18 18:24:48 +0300

    Assignment Complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,17 @@
 // create your App component here
+import React, {useState,useEffect} from "react";
+
+function App() {
+    const [dog, setDog,] = useState(null);
+  
+    useEffect(() => {
+      fetch("https://images.dog.ceo/breeds/bulldog-english/mami.jpg")
+        .then((response) => response.json())
+        .then((data) => {
+          setDog(data.response);
+        });
+    }, []);
+    if(!dog) return <p>Loading...</p>;
+        return <img src={dog} alt="A Random Dog"/>;}
+
+export default App;
